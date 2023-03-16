8c43c637b1799bfa2894c138a07f0d832379712b
DaJon Ewing <dajone37@gmail.com>
2023-03-15 17:03:01 -0700

    lab03 corrections

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-import Modal from 'react-bootstrap/Modal';
-import data from './data/data.json';
 
 
 
